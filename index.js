5bc3863d5b50895e4482171633a6ff83074883c5
Ryan Williams <ryrowi@gmail.com>
2016-06-11 01:37:36 -0400

    add lots more console logging during instantiation

--- index.js
+++ index.js
@@ -17,16 +17,23 @@ var Character = Backbone.Model.extend({
 				'intelligence',
 				'wisdom',
 				'perception',
-				'charisma',
+				'charisma'
 			];
+			var changes = {};
 
 			_.each(attributes, function(attribute) {
-				instance.set(attribute, instance._roll('3d6'));
+				changes[attribute] = instance._roll('3d6');
 			});
+
+			console.log('generating attributes');
+
+			instance.set(changes);
 		})();
 
 		// characteristics
 		var step2 = (function(){
+			console.log('determining characteristics');
+
 			var race = (function() {
 				var races = [
 					'human',
@@ -39,6 +46,9 @@ var Character = Backbone.Model.extend({
 
 				var result = instance._roll('1d6');
 
+				console.log('determining race');
+
+				return instance.set({race: races[result - 1]});
 			})();
 
 			var racial_adjustments = (function() {
@@ -82,16 +92,16 @@ var Character = Backbone.Model.extend({
 					var result = instance._roll('1d8');
 
 					if (_.contains([1, 3, 5], result)) {
-						return instance.set('sex', 'male');
+						return instance.set({sex: 'male'});
 					}
 
 					if (_.contains([2, 4, 6], result)) {
-						return instance.set('sex', 'female');
+						return instance.set({sex: 'female'});
 					}
 
 					if (_.contains([7, 8], result)) {
 						if (is_reroll) {
-							return instance.set('sex', 'intersex');
+							return instance.set({sex: 'intersex'});
 						} else {
 							is_reroll = true;
 							return roll();
@@ -99,47 +109,54 @@ var Character = Backbone.Model.extend({
 					}
 				};
 
+				console.log('determining sex');
+
 				return roll();
 			})();
 
 			var appearance = (function() {
 				var formulas = {
 					'human': {
-						height: '3d6+60',
-						weight: '6d20+100',
+						height:   '3d6+60',
+						weight:   '6d20+100',
 						base_age: '1d8+15'
 					},
 					'ais\'lun': {
-						height: '3d6+36',
-						weight: '5d20+90',
+						height:   '3d6+36',
+						weight:   '5d20+90',
 						base_age: '1d20+30'
 					},
 					'viantu': {
-						height: '3d6+36',
-						weight: '5d20+50',
+						height:   '3d6+36',
+						weight:   '5d20+50',
 						base_age: '1d6+5'
 					},
 					'djenndan': {
-						height: '3d6+84',
-						weight: '6d20+280',
+						height:   '3d6+84',
+						weight:   '6d20+280',
 						base_age: '1d8+12'
 					},
 					'kahlnissá': {
-						height: '3d6+60',
-						weight: '5d20+90',
+						height:   '3d6+60',
+						weight:   '5d20+90',
 						base_age: '1d12+15'
 					},
 					'pulnagá': {
-						height: '3d6+60',
-						weight: '5d20+100',
+						height:   '3d6+60',
+						weight:   '5d20+100',
 						base_age: '1d10+15'
 					}
-				}
+				};
 
-				instance.set('height', instance._roll(formulas[instance.get('race')].height));
-				instance.set('weight', instance._roll(formulas[instance.get('race')].weight));
-				instance.set('base_age', instance._roll(formulas[instance.get('race')].base_age));
+				console.log('determining appearance');
+
+				return instance.set({
+					height:   instance._roll(formulas[instance.get('race')].height),
+					weight:   instance._roll(formulas[instance.get('race')].weight),
+					base_age: instance._roll(formulas[instance.get('race')].base_age)
+				});
 			})();
+
 		})();
 
 		// TODO left off at "Distinguishing Features"
@@ -151,11 +168,17 @@ var Character = Backbone.Model.extend({
 	 */
 	_roll: function(formula) {
 		var rolls = Dice.roll(formula);
-	       var addition = parseInt(formula.split('+')[1]) || 0;
-	       var sum = function(array) {return array.reduce((a, b) => a + b, 0)};
+		var addition = parseInt(formula.split('+')[1]) || 0;
+		var sum = function(array) {return array.reduce((a, b) => a + b, 0)};
 
-	       return sum(rolls) + addition;
-       }
+		return sum(rolls) + addition;
+	},
+
+	set: function(attributes, options) {
+		console.log(attributes);
+
+		return Backbone.Model.prototype.set.call(this, attributes, options);
+	}
 });
 
 http.createServer(function(req, res) {
